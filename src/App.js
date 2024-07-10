707a73d742baf830aa5951790c153c58a7f55276
Safouane <tebibe42@gmail.com>
2024-07-10 06:19:53 +0200

    Final result is here (I'll add some CSS for the third page when I'm home, I focused on the data for this one)

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Home from './pages/Home';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Collection from './pages/Collection';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/collection" element={<Collection />} />
+          <Route path="/search" element={<Search />} />
         </Routes>
       </div>
     </Router>
---
+++ src/pages/Search.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import Navbar2 from '../components/Navbar2';
+
+const Search = () => {
+    const [breed, setBreed] = useState('');
+    const [breedImages, setBreedImages] = useState([]);
+    const [error, setError] = useState('');
+
+    const handleInputChange = (e) => {
+        setBreed(e.target.value);
+    };
+
+    const handleSearch = () => {
+        // If the user doesn't enter the breed name, then we'll stop the function here
+        if (!breed) {
+            setError('Please enter a breed');
+            return;
+        }
+
+        // We clear the variables in order to avoid showing them in screen after the next research
+        setError('');
+        setBreedImages([]);
+
+        // We fetch all the images that affiliate to the breed variable, the name has to be the exact one tho
+        fetch(`https://dog.ceo/api/breed/${breed}/images`)
+            .then(response => {
+                // If the breed name entered exists in the branch (the path is correct), then we'll set the data we got normally, if thr breed is incorrect, we throw an error and catch it after to set it in our variable error
+                if (response.ok === false) {
+                    throw new Error('Breed not found or does not exist');
+                }
+                return response.json();
+            })
+            .then(data => setBreedImages(data.message))
+            .catch(err => setError(err.message));
+    };
+
+    return (
+        <div>
+            <Navbar2/>
+            <h2>Search for the breed you want, if we have it, you adopt it !</h2>
+            <input type="text" value={breed}
+                onChange={handleInputChange}
+                placeholder="Enter the name of the breed"
+            />
+            <button onClick={handleSearch}>Search</button>
+            {/* The error variable is either set or not, if it is, it'll be shown in screen*/}
+            {error && <p>{error}</p>}
+            <div>
+                {breedImages.map((image, index) => (
+                    <img key={index} src={image} alt={`Breed ${breed}`} />
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Search;
